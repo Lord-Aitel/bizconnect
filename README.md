395a64a8c8f14d8bce0b0dd3cad622ebc1aec5aa
Matias Peters <matiaspeters77@gmail.com>
2026-05-03 00:00:20 -0400

    Implemento basico de SplashScreen con logo inicial

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # bizconnect
+<<<<<<< HEAD
 
 A new Flutter project.
 
@@ -15,3 +16,6 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+=======
+Modulo-Dispositivos-Mobiles
+>>>>>>> 1e6fe93485a11121be317cf4f2a671a7207a5235
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:bizconnect/splash_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -30,7 +30,10 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+  '/home': (context) => const HomeScreen(),
+},
+
     );
   }
 }
